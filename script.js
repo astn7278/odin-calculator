68710bf1a4e0e314dbed948113207e41fef6596d
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-09-29 14:26:22 -0700

    Create basic math operation functions

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
     text.style.color = "#" + randomColor;
 });
+
+const add = function(a, b) {
+    let total = a + b;
+    console.log(total);
+}
+
+const subtract = function(a, b) {
+    let total = a - b;
+    console.log(total);
+}
+
+const multiply = function(a, b) {
+    let total = a * b;
+    console.log(total);
+}
+
+const divide = function(a, b) {
+    let total = a / b;
+    console.log(total);
+}
+
