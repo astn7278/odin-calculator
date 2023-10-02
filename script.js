3b2eb662b991df347d3a453a5b0b5481cdf30b42
Austin Fletcher <49542016+astn7278@users.noreply.github.com>
2023-10-01 18:48:08 -0700

    Create main 'operate' function

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     text.style.color = "#" + randomColor;
 });
 
-const add = function(a, b) {
+//Basic Math operators
+const add = function (a, b) {
     let total = a + b;
-    console.log(total);
-}
+    return total;
+};
 
-const subtract = function(a, b) {
+const subtract = function (a, b) {
     let total = a - b;
-    console.log(total);
-}
+    return total;
+};
 
-const multiply = function(a, b) {
+const multiply = function (a, b) {
     let total = a * b;
-    console.log(total);
-}
+    return total;
+};
 
-const divide = function(a, b) {
+const divide = function (a, b) {
     let total = a / b;
-    console.log(total);
-}
+    return total;
+};
 
+//Main operator function
+const num1 = undefined;
+const num2 = undefined;
+const operator = undefined;
+
+const operate = function (num1, num2, operator) {
+    if (operator == add) {
+        return add(num1, num2);
+    } else if (operator == subtract) {
+        return subtract(num1, num2);
+    } else if (operator == multiply) {
+        return multiply(num1, num2);
+    } else if (operator == divide) {
+        return divide(num1, num2);
+    } else return "No Operator";
+};
